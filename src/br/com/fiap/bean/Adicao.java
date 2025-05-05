9e1729a0b30d8bd60585f991f2f780850531002c
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-05-05 09:45:26 -0300

    Classe Adicao incompleta

---
+++ src/br/com/fiap/bean/Adicao.java
@@ -0,0 +1,46 @@
+package br.com.fiap.bean;
+
+public class Adicao {
+    //Atributos
+    private int numInteiro1;
+    private int getNumInteiro2;
+    private double numReal1;
+    private double numReal2;
+    //Contrutores
+    public Adicao() {
+    }
+    //métodos getters/setters
+    public int getNumInteiro1() {
+        return numInteiro1;
+    }
+
+    public void setNumInteiro1(int numInteiro1) {
+        this.numInteiro1 = numInteiro1;
+    }
+
+    public int getGetNumInteiro2() {
+        return getNumInteiro2;
+    }
+
+    public void setGetNumInteiro2(int getNumInteiro2) {
+        this.getNumInteiro2 = getNumInteiro2;
+    }
+
+    public double getNumReal1() {
+        return numReal1;
+    }
+
+    public void setNumReal1(double numReal1) {
+        this.numReal1 = numReal1;
+    }
+
+    public double getNumReal2() {
+        return numReal2;
+    }
+
+    public void setNumReal2(double numReal2) {
+        this.numReal2 = numReal2;
+    }
+    //Métodos de classe (particulares)
+
+}
